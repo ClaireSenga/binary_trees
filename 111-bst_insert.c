763c937ce875e66c69024a7c90a22e64bb604e8e
Geff115 <gabrielnoah129@gmail.com>
2024-06-08 14:42:50 +0100

    Made some changes. Correctly handling insertion in a binary search tree

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 
 /**
  * bst_insert - Inserts a value in a Binary Search Tree.
@@ -14,32 +13,26 @@ bst_t *bst_insert(bst_t **tree, int value)
 	if (tree == NULL)
 		return (NULL);
 
-	/* If the tree is empty, create the root node */
 	if (*tree == NULL)
 	{
-	*tree = binary_tree_node(NULL, value);
-	return (*tree);
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
 	}
-
-	/* Find the correct position to insert the new node */
 	current = *tree;
 	while (current != NULL)
 	{
-	parent = current;
-	if (value < current->n)
-		current = current->left;
-	else if (value > current->n)
-		current = current->right;
-	else
-		return (NULL); /* Value already exists in the tree */
+		parent = current;
+		if (value < current->n)
+			current = current->left;
+		else if (value > current->n)
+			current = current->right;
+		else
+			return (NULL);
 	}
-
-	/* Create the new node */
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Insert the new node as the left or right child */
 	if (value < parent->n)
 		parent->left = new_node;
 	else
